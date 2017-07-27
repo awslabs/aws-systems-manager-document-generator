edb480fefe5cdb7ddd8480281cc7faa1e795f00e
Vladyslav Sitalo <sitalov@amazon.com>
2017-07-27 18:55:35 +0100

    Attempt at processor hierarchy.

--- src/ssm_document_generator/code/preprocessor/processor.py
+++ src/ssm_document_generator/code/preprocessor/processor.py
@@ -1,15 +1,16 @@
-class Preprocessor(object):
+class Processor(object):
     def __init__(self):
-        self.next_processor = None  # Todo noop processor?
+        self.next_processor = None
 
     def process(self, input_data):
-        # do some work and then pass it to the next processor
-        # call some method here that'd be overriden in derived
-        result = input_data
+        result = self._process(input_data)
         if self.next_processor:
             return self.next_processor.process(result)
 
         return result
+
+    def _process(self, input_data):
+        return input_data
 
     def chain(self, next_processor):
         self.next_processor = next_processor
