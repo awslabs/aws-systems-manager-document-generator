4590a4ccefc0be0094126ef04d4debcd3a7d7504
Vladyslav Sitalo <sitalov@amazon.com>
2017-07-27 19:05:10 +0100

    Incorporate stickytape.

--- src/ssm_document_generator/code/python_converter.py
+++ src/ssm_document_generator/code/python_converter.py
@@ -1,11 +1,14 @@
 import json
 
+import stickytape
+
 from ssm_document_generator.code.code_converter import CodeConverter
 
 
 class PythonConverter(CodeConverter):
     COMMAND_TYPE = 'python'
     SHEBANG = '#!/usr/bin/env python'
+    USE_STICKYTAPE = True  # move to config
     # !/rds/bin/opt/redshift/bin/python
 
     def generate_parameters_code(self, parameter_definition):
@@ -13,7 +16,13 @@ class PythonConverter(CodeConverter):
                            for parameter_name in parameter_definition.keys()}
         return ['parameters = ' + json.dumps(parameters_dict)]
 
-    def get_postfix_code(self): # todo consider reading this from file
+    def get_postfix_code(self):  # todo consider reading this from file
         return super().get_postfix_code() + ['print(run_command(parameters))']
+
+    def process_code(self, code_filepath):
+        if self.USE_STICKYTAPE:
+            return stickytape.script(code_filepath).splitlines()
+        else:
+            return super().process_code(code_filepath)
 
 
