c2bced2a8d8ffd71eea2f3b2544a58f52b2c5ef6
Vladyslav Sitalo <sitalov@amazon.com>
2017-07-27 18:17:19 +0100

    Make dmesg return /var/log/messages content.

--- src/ssm_document_generator/examples/dmesg/dmesg.py
+++ src/ssm_document_generator/examples/dmesg/dmesg.py
@@ -1,3 +1,7 @@
 def run_command(parameters):
-    print(parameters)
-    return parameters
+    # print(parameters)
+    # command = 'dmesg | egrep -i \"fail|error|exception|kill|ixgbevf\" | tail -20 | gawk -v uptime=\$( grep btime /proc/stat | cut -d \' \' -f 2 ) \'/^[[ 0-9.]*]/ { print strftime(\"[%Y/%m/%d %H:%M:%S]\", substr(\$0,2,index(\$0,\".\")-2)+uptime) substr(\$0,index(\$0,\"]\")+1) }\''
+    line_limit = parameters['lineLimit']
+
+    # open(encoding='utf-8')
+    return {'result': 'success', 'content': open('/var/log/messages').readlines(), 'parameters': parameters}
