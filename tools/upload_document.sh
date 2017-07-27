e96bb255d3b448affe95a3e4493f9cfede1682a3
Vladyslav Sitalo <sitalov@amazon.com>
2017-07-27 18:16:52 +0100

    Upload script updates to be more generic

--- tools/upload_document.sh
+++ tools/upload_document.sh
@@ -6,17 +6,18 @@
 #pyminifier --bzip2 -o minified.py stickytype_test.py
 
 
-# aws ssm create-document --content file://ssm_to_upload.json --name "generate_test" --document-type "Command" --region us-east-1
-
-documentVersion=$(aws ssm update-document --content file://ssm_to_upload.json --name "generate_test" --document-version "\$LATEST" --output text --query "DocumentDescription.DocumentVersion"  --region us-east-1)
+documentToUpload=../src/ssm_to_upload.json
+# aws ssm create-document --content file://${documentToUpload} --name "generate_test" --document-type "Command" --region us-east-1
+documentVersion=$(aws ssm update-document --content file://${documentToUpload} --name "generate_test" --document-version "\$LATEST" --output text --query "DocumentDescription.DocumentVersion"  --region us-east-1)
 
 aws ssm update-document-default-version --name "generate_test" --document-version "${documentVersion}"  --region us-east-1
 
-invocationId=$(aws ssm send-command --instance-id i-06bd22443bdc58bdf  --document-name "generate_test"  --output text --query "Command.CommandId" --region us-east-1)
+instanceId=i-06bd22443bdc58bdf
+invocationId=$(aws ssm send-command --instance-id ${instanceId}  --document-name "generate_test"  --output text --query "Command.CommandId" --region us-east-1)
 
 sleep 2
 
-aws ssm get-command-invocation --command-id ${invocationId} --instance-id i-06bd22443bdc58bdf --region us-east-1 > command_output
+aws ssm get-command-invocation --command-id ${invocationId} --instance-id ${instanceId} --region us-east-1 > command_output
 cat command_output
 
 #```
