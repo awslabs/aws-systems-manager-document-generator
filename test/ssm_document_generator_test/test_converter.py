71277c05dd3ea5aa171f1a4edb4553c975b82974
Vladyslav Sitalo <sitalov@amazon.com>
2017-08-09 12:14:33 +0100

    Converter add cf/directory tests.
    cr https://code.amazon.com/reviews/CR-491188

--- test/ssm_document_generator_test/test_converter.py
+++ test/ssm_document_generator_test/test_converter.py
@@ -1,6 +1,8 @@
 from unittest import mock
 from unittest.mock import MagicMock
+import pytest
 
+from troposphere import Template
 
 from ssm_document_generator.converter import Converter
 
@@ -14,3 +16,28 @@ def test_convert_sanity(yaml_mock, converter_factory_mock):
 
     Converter.convert(definition_path_mock)
     converter_factory_mock.get_converter().convert.assert_called_once()
+
+
+@pytest.mark.parametrize('test_input, expected', [
+    ([], Template().to_dict()),
+    ([Converter.ConversionResult({}, {'name': 'tname'})],
+     {'Resources': {'tname': {
+         'Type': 'AWS::SSM::Document',
+         'Properties': {'Content': {}, 'DocumentType': 'Command'}
+     }}})
+])
+def test_to_cloudformation(test_input, expected):
+    assert Converter.to_cloudformation(test_input).to_dict() == expected
+
+
+@pytest.mark.parametrize('discovered, converted, expected', [
+    ([], [], []),
+    (['mock.def'], 'mock_return', ['mock_return']),
+])
+@mock.patch('ssm_document_generator.converter.Converter.convert')
+@mock.patch('ssm_document_generator.converter.discovery')
+def test_convert_directory(discovery_mock, convert_mock, discovered, converted, expected):
+    discovery_mock.get_definitions.return_value = discovered
+    convert_mock.return_value = converted
+
+    assert Converter.convert_directory(directory_path='dummy_path') == expected
