90f2be44ff328a3a30be399d72b7ffbcf2a116cc
Vladyslav Sitalo <sitalov@amazon.com>
2017-08-16 16:21:16 +0100

    Fix elements limits

--- src/ssm_document_generator/examples/get_file/get_file.py
+++ src/ssm_document_generator/examples/get_file/get_file.py
@@ -24,4 +24,4 @@ def run_command(parameters):
     exclude_filter_list = ['dhclient']
     filtered = list(exclude_filter(filtered, exclude_filter_list))
 
-    return {'status': 'success', 'file_content': filtered[-int(parameters['lineLimit']):-1]}
+    return {'status': 'success', 'file_content': filtered[-int(parameters['lineLimit']):]}
