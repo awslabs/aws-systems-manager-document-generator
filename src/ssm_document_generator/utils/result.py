4e9a786f53443d854928f24894293f00304c5f6d
Vladyslav Sitalo <sitalov@amazon.com>
2017-08-28 11:38:34 +0100

    Implement Result util.

---
+++ src/ssm_document_generator/utils/result.py
@@ -0,0 +1,25 @@
+class Result:
+    """
+    Util for simplifying providing unified interface to generated commands
+    """
+
+    @classmethod
+    def success(cls, result_data):
+        return {'status': 'success', 'result': result_data}
+
+    @classmethod
+    def failure(cls, error, message=None):
+        return {'status': type(error).__name__, 'message': message if message else str(error)}
+
+    @classmethod
+    def run(cls, runnable):
+        """
+        Try running passed runnable, if run is successful return Result.success if run threw some exception
+        catch it and return Result.failure.
+        If functions has parameters - you should pass a lambda with the function call. It's done like that
+        so that if any exceptions arise at the parameter evaluation time - they would be caught.
+        """
+        try:
+            return Result.success(runnable())
+        except Exception as error:
+            return Result.failure(error)
---
+++ test/ssm_document_generator_test/code/utils/test_result.py
@@ -0,0 +1,35 @@
+import pytest
+
+from ssm_document_generator.utils.result import Result
+
+
+@pytest.mark.parametrize('test_input', [
+    [],
+    [1, 2],
+    {'foo': 'bar'}
+])
+def test_success(test_input):
+    assert Result.success(test_input) == {'status': 'success', 'result': test_input}
+
+
+@pytest.mark.parametrize('error, message, expected', [
+    (RuntimeError('tm1'), None, {'status': 'RuntimeError', 'message': 'tm1'}),
+    (RuntimeError('tm1'), 'tm2', {'status': 'RuntimeError', 'message': 'tm2'}),
+])
+def test_failure(error, message, expected):
+    assert Result.failure(error, message) == expected
+
+
+def raiser(exception):
+    """
+    Need this to work around limitation of the fact that I can't have just a statement in lambda
+    """
+    raise exception
+
+
+@pytest.mark.parametrize('runnable, expected', [
+    (lambda: [], Result.success([])),
+    (lambda: raiser(RuntimeError('t1')), Result.failure(RuntimeError('t1')))
+])
+def test_run(runnable, expected):
+    assert Result.run(runnable) == expected
