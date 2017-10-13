24d14bc30417ffd0c765bda13af8ae84e82239eb
Vladyslav Sitalo <sitalov@amazon.com>
2017-10-13 16:42:14 +0100

    Introduce common parameters.

---
+++ src/ssm_document_generator/definition/parameters/common.py
@@ -0,0 +1,7 @@
+from ssm_document_generator.definition.parameters.parameter import Parameter
+
+ENTITIES_LIMIT = Parameter(name='entitiesLimit',
+                           description='Maximum number of things (lines, objects, etc) to return.\n'
+                                       'Only last {{lineLimit}} lines would be returned.\n'
+                                       'Please specify 0 for no limit.',
+                           default='0')
