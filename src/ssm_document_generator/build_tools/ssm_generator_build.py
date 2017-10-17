8758eed8cfd51125e9a0c62da6b35c8098a59c2a
Vladyslav Sitalo <sitalov@amazon.com>
2017-10-17 18:59:45 +0100

    Change SSMGeneratorBuild to allow for using modules defined in same package as command definitions.

--- src/ssm_document_generator/build_tools/ssm_generator_build.py
+++ src/ssm_document_generator/build_tools/ssm_generator_build.py
@@ -1,15 +1,21 @@
+import sys
+
 from pathlib import Path
+from setuptools.command.install_scripts import install_scripts
 
-from setuptools.command import build_py
 
-
-class SSMGeneratorBuild(build_py.build_py):
+class SSMGeneratorBuild(install_scripts):
     """
     Defines override of build_py step that has ssm_generator step as prerequisite.
     Use this to build CloudFormation template from your generated SSM documents.
     """
 
     def run(self):
-        Path('build/cloudFormation').mkdir(parents=True, exist_ok=True)
-        self.run_command('ssm_generator')
         super().run()
+        Path('build/cloudFormation').mkdir(parents=True, exist_ok=True)
+
+        # This is needed if we want to use the modules defined in the same package as definition, as the
+        # directory for the module does not exist initially - the path_importer stored in cache is None
+        # And so we need to clean cache to recreate the importers.
+        sys.path_importer_cache.clear()
+        self.run_command('ssm_generator')
